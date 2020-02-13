14ce582dc50a0f03400e932f9d11fe8affa436ff
guiguitcho <guillaume.maitrejean@gmail.com>
2020-02-13 18:23:06 +0100

    save

---
+++ Encre/DP_tau.py
@@ -0,0 +1,32 @@
+import matplotlib
+matplotlib.rcParams['text.usetex'] = True
+import matplotlib.pyplot as plt
+import numpy as np
+
+plt.rcParams.update({'font.size': 18})
+plt.rcParams.update({'figure.autolayout': True})
+
+fig = plt.figure()
+
+
+def f(x):
+    return 0.2*x+2.7
+
+
+
+x1 = np.arange(0, 1, 0.01)
+
+
+exp_x,exp_y = np.loadtxt('./Encre/DP_tau.txt', delimiter=';', unpack=True)
+
+plt.plot(x1, f(x1), '-k')
+plt.plot(exp_x, exp_y, 'ob', label='$P_{tank}$')
+
+plt.legend()
+# plt.tight_layout()
+
+plt.xlabel('$\\tau_e (\mu s)$')
+plt.ylabel('$\Delta P(bar)$')
+plt.grid(True)
+plt.savefig("dP.eps", dpi=300)
+plt.show()
---
+++ Encre/LbInk.py
@@ -0,0 +1,39 @@
+import matplotlib
+matplotlib.rcParams['text.usetex'] = True
+import matplotlib.pyplot as plt
+import numpy as np
+from scipy import interpolate
+import scipy.optimize as opt
+
+plt.rcParams.update({'font.size': 18})
+plt.rcParams.update({'figure.autolayout': True})
+
+# This is the function we are trying to fit to the data.
+def func(x, a, b, c):
+     return a * np.exp(-b * x) + c
+
+
+fig = plt.figure()
+
+r1,r2,r3,r4,r5,r6,r7 = np.loadtxt('./Encre/LB_9651_GS_k', delimiter=';',skiprows=2, unpack=True)
+
+# # Spline
+# tck,u     = interpolate.splprep( [r6,r4] ,s = 0 )
+# xnew,ynew = interpolate.splev( np.linspace( 0, 1, 62 ), tck,der = 0)
+
+# The actual curve fitting happens here
+optimizedParameters, pcov = opt.curve_fit(func, r6, r4)
+
+
+
+# plt.plot( xnew ,ynew, '-k')
+
+plt.plot(r6, func(r6, *optimizedParameters), '-k')
+plt.plot(r6, r4, 'ob', label='$\mathcal{L}_{exp}^b$')
+
+plt.legend()
+
+plt.xlabel('Disturbance amplitude ($V$)')
+plt.ylabel('Breakup length ($mm$)')
+plt.savefig("LbInk.eps", dpi=300)
+plt.show()
